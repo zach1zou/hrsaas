8faac59ad79b8a50a6daeaf50d221b8b62be5cbd
zach1zou <791522322@qq.com>
2022-08-02 14:54:13 +0800

    完成sidebar样式修改

--- src/router/index.js
+++ src/router/index.js
@@ -172,6 +172,10 @@ const createRouter = () => new Router({
 
 const router = createRouter()
 
+
+
+
+
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
   const newRouter = createRouter()
